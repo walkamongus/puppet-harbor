da3221a1639a739f1e830a5706fb22aa512d7d60
Chadwick Banning <walkamongus@users.noreply.github.com>
2019-12-13 15:26:58 -0500

    Rubocop fixes

--- lib/facter/harbor_systeminfo.rb
+++ lib/facter/harbor_systeminfo.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'openssl'
 require 'json'
 require 'yaml'
 
 Facter.add(:harbor_systeminfo) do
-  confine { File.exists? '/opt/harbor/harbor.yml' }
+  confine { File.exist? '/opt/harbor/harbor.yml' }
   setcode do
-    cnf  = YAML::load(File.open('/opt/harbor/harbor.yml'))
+    cnf  = YAML.safe_load(File.open('/opt/harbor/harbor.yml'))
     url  = "http://#{cnf['hostname']}/api/systeminfo"
     uri  = URI.parse(url)
     begin
       resp = Net::HTTP.new(uri.host).request(Net::HTTP::Get.new(uri))
-      if resp['location'] != nil
+      unless resp['location'].nil?
         uri = URI.parse(resp['location'])
         https = Net::HTTP.new(uri.host, uri.port)
         https.use_ssl = true
@@ -21,7 +23,7 @@ Facter.add(:harbor_systeminfo) do
       end
       data = JSON.parse(resp.body)
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-              Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError
+           Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError
       data = {}
     end
     data
